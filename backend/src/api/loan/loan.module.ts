7fc9cd3c884c1c0a5273fad902a9e413dff902e9
yizunwong <yizunwong@gmail.com>
2025-08-10 14:54:36 +0800

    remove unused code

--- backend/src/api/loan/loan.module.ts
+++
@@ -1,9 +0,0 @@
-// import { Module } from '@nestjs/common';
-// import { LoanService } from './loan.service';
-// import { LoanController } from './loan.controller';
-
-// @Module({
-//   controllers: [LoanController],
-//   providers: [LoanService],
-// })
-// export class LoanModule {}
--- blockchain/ignition/modules/LoanModule.ts
+++
@@ -1,9 +0,0 @@
-// ignition/modules/LoanModule.ts
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const LoanModule = buildModule("LoanModule", (m) => {
-  const loanContract = m.contract("LoanContract", []);
-  return { loanContract };
-});
-
-export default LoanModule;
