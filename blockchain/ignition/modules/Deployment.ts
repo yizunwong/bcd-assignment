69319745d1c62ca8491cee0489dd8d8e7c5e4b12
yizunwong <138889025+yizunwong@users.noreply.github.com>
2025-08-15 15:41:49 +0800

    chore: extend deployment and frontend export

--- blockchain/ignition/modules/Deployment.ts
+++ blockchain/ignition/modules/Deployment.ts
@@ -1,10 +1,20 @@
-// ignition/modules/InsuranceContract.ts
+// ignition/modules/Deployment.ts
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("Deployment", (m) => {
-  // Deploy InsuranceContract (no constructor args)
-  const insurance = m.contract("InsuranceContract");
+const FULL_SUPPLY = 100_000n * 10n ** 18n;
 
-  // You can export anything you want to reference later
-  return { insurance };
+export default buildModule("Deployment", (m) => {
+  // Deploy Coverly ERC20 token
+  const token = m.contract("CoverlyToken");
+
+  // Deploy ICO contract that will sell Coverly tokens
+  const ico = m.contract("ICO", [token]);
+
+  // Transfer all tokens to the ICO contract for distribution
+  m.call(token, "transfer", { args: [ico, FULL_SUPPLY], after: [ico] });
+
+  // Deploy InsuranceContract which uses the Coverly token for payments
+  const insurance = m.contract("InsuranceContract", [token]);
+
+  return { token, ico, insurance };
 });
